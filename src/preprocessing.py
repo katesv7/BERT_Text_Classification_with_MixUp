bf631de5c800481d58af48b45969ae1c654fdb2f
katesv7 <96252099+katesv7@users.noreply.github.com>
2024-06-12 17:28:19 +0300

    Add files via upload

---
+++ src/preprocessing.py
@@ -0,0 +1,7 @@
+def preproc():
+    pass
+
+
+if __name__ == "__main__":
+    preproc()
+    
---
+++ src/train.py
@@ -0,0 +1,14 @@
+def train_model(batch: int, epochs: int) -> None:
+    """
+    Функция для запуска тренировки модели.
+    Результат сохрняется в файл.
+
+    Args:
+        batch (int): Бач сайз дя обучения
+        epochs (int): Кол-во эпох в обучении
+    """
+    return 0
+
+
+if __name__ == "__main__":
+    train_model(batch=8, epochs=100)
