322f9ed2e4f783faf3a7ce2134e51848a614df2d
katesv7 <96252099+katesv7@users.noreply.github.com>
2024-06-12 17:41:13 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,16 @@
-import cv2
-
-from src.EDA import EDA
+from src.preprocessing import preprocess_data
 from src.train import train_model
-
+from src.evaluate import evaluate_model
 
 def main():
-    EDA()
-    train_model(8, 100)
+    # Предобработка данных
+    preprocess_data()
 
+    # Обучение модели
+    train_model(batch_size=8, epochs=3)
+
+    # Оценка модели
+    evaluate_model()
 
 if __name__ == "__main__":
     main()
-
