32d6dbebdf92db5f99a50bc92b416edd915ccfe0
katesv7 <96252099+katesv7@users.noreply.github.com>
2024-06-14 21:41:17 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,15 +2,12 @@ from src.preprocessing import preprocess_data
 from src.train import train_model
 from src.evaluate import evaluate_model
 
+
 def main():
-    # Предобработка данных
     preprocess_data()
-
-    # Обучение модели
     train_model(batch_size=8, epochs=3)
-
-    # Оценка модели
     evaluate_model()
+
 
 if __name__ == "__main__":
     main()
